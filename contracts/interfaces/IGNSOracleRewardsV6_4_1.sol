d55a654602f8134bde78c25af1f1ec26a92891f6
trong.dang <dtrong97vn@gmail.com>
2024-01-11 21:36:21 +0700

    feat: add GNSTradingV6_4_1.sol

---
+++ contracts/interfaces/IGNSOracleRewardsV6_4_1.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {StorageInterfaceV5} from "./StorageInterfaceV5.sol";
+
+interface IGNSOracleRewardsV6_4_1 {
+    struct TriggeredLimitId {
+        address trader;
+        uint pairIndex;
+        uint index;
+        StorageInterfaceV5.LimitOrder order;
+    }
+    enum OpenLimitOrderType {
+        LEGACY,
+        REVERSAL,
+        MOMENTUM
+    }
+
+    function storeTrigger(TriggeredLimitId calldata) external;
+
+    function unregisterTrigger(TriggeredLimitId calldata) external;
+
+    function distributeOracleReward(TriggeredLimitId calldata, uint) external;
+
+    function openLimitOrderTypes(address, uint, uint) external view returns (OpenLimitOrderType);
+
+    function setOpenLimitOrderType(address, uint, uint, OpenLimitOrderType) external;
+
+    function triggered(TriggeredLimitId calldata) external view returns (bool);
+
+    function timedOut(TriggeredLimitId calldata) external view returns (bool);
+}
