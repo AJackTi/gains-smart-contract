8918e032eee2e31b86abc392a1d9040e29b2a3d9
trong.dang <dtrong97vn@gmail.com>
2024-01-11 21:28:25 +0700

    feat: add GNSPriceAggregatorV6_4.sol

---
+++ contracts/interfaces/CallbacksInterfaceV6_4.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+pragma abicoder v2;
+
+interface CallbacksInterfaceV6_4 {
+    struct AggregatorAnswer {
+        uint orderId;
+        uint price;
+        uint spreadP;
+        uint open;
+        uint high;
+        uint low;
+    }
+
+    function openTradeMarketCallback(AggregatorAnswer memory) external;
+
+    function closeTradeMarketCallback(AggregatorAnswer memory) external;
+
+    function executeNftOpenOrderCallback(AggregatorAnswer memory) external;
+
+    function executeNftCloseOrderCallback(AggregatorAnswer memory) external;
+}
